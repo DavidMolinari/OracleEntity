48562c2d934b3c5588f43be2e780c2f6a56bcd9f
MOLINARI David <molinari.d@sio.lan>
2016-10-05 12:46:59 +0200

    Copie de @Hirokilen

--- OracleEntityF/Program.cs
+++ OracleEntityF/Program.cs
@@ -10,6 +10,101 @@ namespace OracleEntityF
     {
         static void Main(string[] args)
         {
+            using (OracleEntities oracleContexte = new OracleEntities())
+            {
+                var requeteEmployes = from EMPLOYE in oracleContexte.EMPLOYEs
+                                      select EMPLOYE;
+                var lesEmployes = requeteEmployes.ToList();
+
+                foreach (var unEmploye in lesEmployes)
+                {
+                    Console.WriteLine(unEmploye.NUMEMP + " - " + unEmploye.NOMEMP);
+                }
+
+                Console.WriteLine("---------------------------------------------------------");
+
+                var unCodeProjet = "PR1";
+                var requeteEmployesProjet = from EMPLOYE in oracleContexte.EMPLOYEs
+                                            where EMPLOYE.CODEPROJET.TrimEnd() == unCodeProjet
+                                            select EMPLOYE;
+                lesEmployes = requeteEmployesProjet.ToList();
+
+                foreach (var unEmploye in lesEmployes)
+                {
+                    Console.WriteLine(unEmploye.NUMEMP + " - " + unEmploye.NOMEMP);
+                }
+
+                Console.WriteLine("---------------------------------------------------------");
+                var idEmploye = 33;
+
+                var requeteEmployesById = from EMPLOYE in oracleContexte.EMPLOYEs
+                                          where EMPLOYE.NUMEMP== idEmploye
+                                          select EMPLOYE;
+                var empplyeId = requeteEmployesById.FirstOrDefault();
+                if (empplyeId != null)
+                {
+                    Console.WriteLine(empplyeId.NOMEMP + " - " + empplyeId.PRENOMEMP + " - " + empplyeId.SALAIRE);
+
+                }
+                else Console.WriteLine("L'employé : " + idEmploye + " n'existe pas");
+
+                Console.WriteLine("---------------------------------------------------------");
+
+
+                var requete = from s in oracleContexte.SEMINAIREs
+                              join COUR in oracleContexte.COURS on s.CODECOURS equals COUR.CODECOURS
+                              group s by new { s.CODECOURS, s.COUR.LIBELLECOURS } into groupeEmployes
+                              select new
+                              {
+                                  LibelleCours = groupeEmployes.Key.LIBELLECOURS,
+                                  Cours = groupeEmployes.Key.CODECOURS,
+                                  Nombre = groupeEmployes.Count()
+                              };
+
+                var lesSeminaires = requete.ToList();
+
+                foreach (var unSeminaire in lesSeminaires)
+                {
+                    Console.WriteLine(unSeminaire.LibelleCours + " - " + unSeminaire.Cours + " - " + unSeminaire.Nombre);
+                    var requeteDate = from s in oracleContexte.SEMINAIREs
+                                      where s.CODECOURS == unSeminaire.Cours
+                                      select s;
+                    var lesDates = requeteDate.ToList();
+                    foreach (var uneDate in lesDates)
+                    {
+                        Console.WriteLine + "         " + uneDate.DATEDEBUTSEM);
+                    }
+                }
+
+                 // Mise à jour des données
+                /*
+                var emp = oracleContexte.EMPLOYEs.Find(200);
+                
+                
+                if (emp != null)
+                {
+                    emp.SALAIRE = emp.SALAIRE * (decimal)1.1;
+                    Console.WriteLine("Le nouveau salaire = " + emp.SALAIRE);
+                    oracleContexte.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("L'employé n'existe pas.");
+                }
+                */
+                // CREATION D'UN COUR
+                /*
+                COUR unCours = new COUR();
+                unCours.CODECOURS = "BR099";
+                unCours.LIBELLECOURS = "Entity Framework 6 avec Oracle";
+                unCours.NBJOURS = 4;
+                oracleContexte.COURS.Add(unCours);
+                oracleContexte.SaveChanges();
+                Console.WriteLine("Le cours a été créé");
+                */
+
+            }
+            Console.ReadLine();
         }
     }
 }
